844c00929ba4c4a060d831c0d53c091e9899ca04
jjlmoya <juanjo1989@gmail.com>
2026-04-18 21:28:55 +0200

    fix: remove re-exports of removed component vars from main index

--- src/index.ts
+++ src/index.ts
@@ -17,13 +17,6 @@ export type {
 
 export { ALL_ENTRIES, ALL_TOOLS } from './tools';
 
-export { SafeZonesComponent, SafeZonesSEO, SafeZonesBibliography } from './tool/safeZones';
-export { YoutubeThumbnailComponent, YoutubeThumbnailSEO, YoutubeThumbnailBibliography } from './tool/youtubeThumbnail';
-export { SocialValueCalculatorComponent, SocialValueCalculatorSEO, SocialValueCalculatorBibliography } from './tool/socialValueCalculator';
-export { RedditFormatterComponent, RedditFormatterSEO, RedditFormatterBibliography } from './tool/redditFormatter';
-export { SocialImageResizerComponent, SocialImageResizerSEO, SocialImageResizerBibliography } from './tool/socialImageResizer';
-export { TinderPhotoOptimizerComponent, TinderPhotoOptimizerSEO, TinderPhotoOptimizerBibliography } from './tool/tinderPhotoOptimizer';
-export { YoutubeThumbnailPreviewerComponent, YoutubeThumbnailPreviewerSEO, YoutubeThumbnailPreviewerBibliography } from './tool/youtubeThumbnailPreviewer';
 
 export { SAFE_ZONES_TOOL } from './tool/safeZones/index';
 export { YOUTUBE_THUMBNAIL_TOOL } from './tool/youtubeThumbnail/index';
