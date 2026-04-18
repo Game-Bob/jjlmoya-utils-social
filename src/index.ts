a53ded24894af4bc101c2a0275eb7254d0ddfd4d
jjlmoya <juanjo1989@gmail.com>
2026-04-18 19:45:33 +0200

    feat: add ALL_ENTRIES export

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ export type {
   ToolDefinition,
 } from './types';
 
-export { ALL_TOOLS } from './tools';
+export { ALL_ENTRIES, ALL_TOOLS } from './tools';
 
 export { SafeZonesComponent, SafeZonesSEO, SafeZonesBibliography } from './tool/safeZones';
 export { YoutubeThumbnailComponent, YoutubeThumbnailSEO, YoutubeThumbnailBibliography } from './tool/youtubeThumbnail';
